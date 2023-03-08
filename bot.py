3bdc304db3e9d144f44dc6d11a8912a5abfdb681
Kitaura in work <kitahide12123@gmail.com>
2023-03-08 13:53:11 +0900

    first-commit

---
+++ bot.py
@@ -0,0 +1,34 @@
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.environ.get("OPENAI_SECRET_KEY")
+
+
+class Bot:
+
+    def __init__(self, *args):
+        self.system_messages = [
+            {"role": "system", "content": text}
+            for text in args
+        ]
+
+    def talk(self, message: str, /) -> str:
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                *self.system_messages,
+                {"role": "user", "content": message}
+            ]
+        )
+        return completion.choices[0].message.content
+    
+
+cute_bot = Bot(
+    "語尾ににゃんをつけてください。",
+    "可愛く答えてください。"
+)
+print(
+    cute_bot.talk("正しい箸の持ち方を教えて")
+)
