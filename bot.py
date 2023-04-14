f99bc2b6ec025c7927bcea908d2c7c2d98f00f67
Kitaura in work <kitahide12123@gmail.com>
2023-04-14 15:58:22 +0900

    Bot version 0

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import openai
+from openai.error import RateLimitError
 import os
 from dotenv import load_dotenv
 
@@ -8,35 +9,32 @@ openai.api_key = os.environ.get("OPENAI_SECRET_KEY")
 
 class Bot:
 
-    def __init__(self, message: str):
-        self.completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"rolw": "user", "content": message}
-            ]
-        )
-
-    def system(self, *args):
-        self.system_messages = [
-            {"role": "system", "content": text}
-            for text in args
+    def __init__(self, *args):
+        self.chat_logs = [
+            {"role": "system", "content": arg}
+            for arg in args
         ]
 
-    def chat(self, message: str, /) -> str:
+    def chat(self, request: str, /) -> str:
+        self.chat_logs.append({"role": "user", "content": request})
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=[
-                *self.system_messages,
-                {"role": "user", "content": message}
-            ]
+            messages=self.chat_logs
         )
-        return completion.choices[0].message.content
-    
+        reply = completion.choices[0].message.content
+        self.chat_logs.append({"role": "assistant", "content": reply})
+        return reply
 
-cute_bot = Bot(
-    "語尾ににゃんをつけてください。",
-    "可愛く答えてください。"
-)
-print(
-    cute_bot.talk("正しい箸の持ち方を教えて")
-)
+
+if __name__ == "__main__":
+    bot = Bot(
+        "出来る限り１００文字以内に返答してください。"
+    )
+    print("好きな質問をしてください。Enterで終了します。")
+
+    while True:
+        request = input("\n>>")
+        if not request:
+            print("終了します。")
+            break
+        print(bot.chat(request))
