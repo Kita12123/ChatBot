fbb6949d9f350c5f688807a5de6b1ccb2adf3cf9
Kitaura in work <kitahide12123@gmail.com>
2023-05-25 17:05:49 +0900

    *

--- apps/bot.py
+++ apps/bot.py
@@ -17,11 +17,14 @@ class Bot:
 
     def chat(self, request: str) -> str:
         self.history.append({"role": "user", "content": request})
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=self.history
-        )
-        reply = completion.choices[0].message.content
+        try:
+            completion = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=self.history
+            )
+            reply = completion.choices[0].message.content
+        except (openai.error.RateLimitError):
+            reply = "ごめんなさい。エラーが発生しました。\n時間をおいてからもう一度お願いします。"
         self.history.append({"role": "assistant", "content": reply})
         return reply
 
